491a909d1c2e572d9ba493f9de4ddcf3e720ef47
nour <nouraljasim@gmail.com>
2023-10-08 11:22:29 +0300

    server twilio set up

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ require('dotenv').config();
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-// const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
-// const twilioClient = require('twilio')(accountSid, authToken);
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
+const twilioClient = require('twilio')(accountSid, authToken);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,6 +22,26 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   // Your existing code for Twilio integration
+  const { message, user: sender , type, members } = req.body;
+
+  if (type === 'message.new') {
+    members
+      .filter((member) => member.user_id !== sender.id)
+      .forEach(({ user }) => {
+        if (!user.online) {
+          twilioClient.messages.create({
+            body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+            messagingServiceSid: messagingServiceSid,
+            to: user.phoneNumber
+          })
+          .then(() => console.log('Message sent!'))
+          .catch((err) => console.log(err));
+        }
+      });
+    return res.status(200).send('Message sent!');
+  }
+  return res.status(200).send('Not a new message request');
+
 });
 
 // Enable CORS for the /auth route
